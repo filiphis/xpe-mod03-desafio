fa53a90594c5ba3978d8f10d32a9b059bb2c3b0f
Luiz Felipe da Silveira <l.filiphis@hotmail.com>
2023-03-31 01:01:53 -0300

    Adicionando a tabela e os selects

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,91 @@
 import { useEffect, useState } from "react";
 import { despesa } from "./api/types";
 import { getDespesasEndpoint } from "./api/backend";
+import { ListaDespesas } from "./components/ListaDespesas";
+import { Box } from "@mui/system";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 
 function App() {
   const [despesas, setDespesas] = useState<despesa[]>([]);
+  const [year, setYear] = useState("2020");
+  const [month, setMonth] = useState("06");
 
   useEffect(() => {
     async function fetchData() {
+      // const ano = "2021";
+      const mes = "06";
       const response = await getDespesasEndpoint(
-        "http://localhost:3001/despesas"
+        `http://localhost:3001/despesas?mes=${year}-${month}`
       );
+      // `http://localhost:3001/despesas?mes=${year}-${mes}&_sort=mes,dia`
       if (response?.statusText === "OK") {
         const { data } = response;
-        console.log(data);
+        const newDespesas = data;
+        setDespesas(newDespesas);
       }
     }
 
     fetchData();
-  }, []);
+  }, [year, month]);
+
+  function yearHandleChange(event: SelectChangeEvent) {
+    const newYear = event.target.value;
+    setYear(newYear);
+  }
+
+  function monthHandleChange(event: SelectChangeEvent) {
+    const newMonth = event.target.value;
+    setMonth(newMonth);
+  }
+  console.log(year);
 
   return (
     <>
-      <h1>teste</h1>
+      <Box
+        sx={{
+          minWidth: 120,
+          marginTop: 8,
+          display: "flex",
+          justifyContent: "start",
+          gap: 5,
+          paddingLeft: "5%",
+        }}
+      >
+        <FormControl sx={{ minWidth: 120 }}>
+          <InputLabel id="year">Ano</InputLabel>
+          <Select
+            labelId="year"
+            id="year"
+            value={year}
+            label="Ano"
+            onChange={yearHandleChange}
+          >
+            <MenuItem value={2020}>2020</MenuItem>
+            <MenuItem value={2021}>2021</MenuItem>
+          </Select>
+          {year}
+        </FormControl>
+
+        <FormControl sx={{ minWidth: 120 }}>
+          <InputLabel id="month">Mês</InputLabel>
+          <Select
+            labelId="month"
+            id="month"
+            value={month}
+            label="Mes"
+            onChange={monthHandleChange}
+          >
+            <MenuItem value={"06"}>06</MenuItem>
+            <MenuItem value={"11"}>11</MenuItem>
+          </Select>
+          {month}
+        </FormControl>
+      </Box>
+
+      <ListaDespesas despesas={despesas} />
     </>
   );
 }
--- src/components/ListaDespesas/index.tsx
+++ src/components/ListaDespesas/index.tsx
@@ -6,6 +6,8 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { despesa } from "../../api/types";
+import { Box } from "@mui/system";
 
 function createData(
   id: number,
@@ -17,44 +19,66 @@ function createData(
   return { id, descricao, categoria, dia, valor };
 }
 
-const rows = [
-  createData(1, "Frozen yoghurt", "159", "6.0", 24),
-  createData(2, "Ice cream sandwich", "237", "9.0", 37),
-  createData(3, "Eclair", "262", "16.0", 24),
-  createData(4, "Cupcake", "305", "3.7", 67),
-  createData(5, "Gingerbread", "356", "16.0", 49),
-];
+// const rows = [
+//   createData(1, "Frozen yoghurt", "159", "6.0", 24),
+//   createData(2, "Ice cream sandwich", "237", "9.0", 37),
+//   createData(3, "Eclair", "262", "16.0", 24),
+//   createData(4, "Cupcake", "305", "3.7", 67),
+//   createData(5, "Gingerbread", "356", "16.0", 49),
+// ];
 
-export default function BasicTable() {
+type ListaDespesasProps = {
+  despesas: despesa[];
+};
+
+console.log();
+function ListaDespesas({ despesas }: ListaDespesasProps) {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.id}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.descricao}
-              </TableCell>
-              <TableCell align="right">{row.descricao}</TableCell>
-              <TableCell align="right">{row.categoria}</TableCell>
-              <TableCell align="right">{row.dia}</TableCell>
-              <TableCell align="right">{row.valor}</TableCell>
+    <Box
+      sx={{
+        minWidth: 650,
+        borderColor: "grey.900",
+        display: "flex",
+        justifyContent: "center",
+        marginY: 10,
+        boxShadow: 3,
+      }}
+    >
+      <TableContainer
+        component={Paper}
+        sx={{
+          minWidth: 650,
+          maxWidth: "90%",
+        }}
+      >
+        {console.log("despesas", despesas)}
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="left">Despesa</TableCell>
+              <TableCell align="left">Categoria</TableCell>
+              <TableCell align="left">Dia</TableCell>
+              <TableCell align="center">Valor (R$)</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {despesas.map((despesa) => (
+              <TableRow
+                hover
+                key={despesa.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell align="left">{despesa.descricao}</TableCell>
+                <TableCell align="left">{despesa.categoria}</TableCell>
+                <TableCell align="left">{despesa.dia}</TableCell>
+                <TableCell align="center">{despesa.valor}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 }
+
+export { ListaDespesas };
